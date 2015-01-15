4888fc274b59c28ed1ba72e97eacfb82dcf155b7
znotdead <zhirafchik@gmail.com>
2015-01-15 23:55:04 +0800

    deploy

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,17 @@ $(document).ready(function(){
     $('table').addClass('table');
     $('table').addClass('table-striped');
     $('table').tablesorter();
+    // for bootstrap css (display borders)
+    $($('div.container')[1]).find('img').addClass('img-thumbnail');
+
+    // hover
+    $($('div.container')[1]).find('img').addClass('thumbnail');
+    $('.thumbnail').hover(function(){
+        $(this).addClass('fullsizeimage');
+        $(this).removeClass('thumbnail');
+    },
+    function(){
+        $(this).addClass('thumbnail');
+        $(this).removeClass('fullsizeimage');
+    });
 });
